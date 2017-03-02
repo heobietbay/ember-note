410ecf12ee0124a9f7d7e4e4da10532a234e8476
Khoa Tran <trandangkhoa@tma.com.vn>
2017-03-02 15:50:12 +0700

    Render note template to Main route, instead of render as nested template"

--- app/notebooks/view/notes/route.js
+++ app/notebooks/view/notes/route.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    renderTemplate: function() {
+        this.render('notebooks/view/notes', {
+            into: 'application'
+        });
+    },
     actions: {
         close: function() {
-        	let model = this.modelFor(this.routeName);
-        	let parentNotebookId = model.get('notebook').get('id');
-            this.transitionTo('notebooks.view',parentNotebookId);
+            let model = this.modelFor(this.routeName);
+            let parentNotebookId = model.get('notebook').get('id');
+            this.transitionTo('notebooks.view', parentNotebookId);
         }
     }
 });
