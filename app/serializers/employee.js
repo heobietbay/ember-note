e627a069183960d68e48fc6fdcf875270418aabd
Khoa Tran <trandangkhoa@tma.com.vn>
2017-03-09 15:22:29 +0700

    Override the keyForRelationship, and simply return the passed in key.

--- app/serializers/employee.js
+++ app/serializers/employee.js
@@ -3,24 +3,12 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
     primaryKey: 'id',
 
-    serialize: function(snapshot, options) {
-        var payLoadContent = this._super(snapshot, options).data;
-        if (payLoadContent.relationships) {
-            let relationships = payLoadContent.relationships;
-            if (relationships["employee-type"]) {
-            	var employeeType = relationships["employee-type"].data;
-                //relationships["employeetype"] = relationships["employee-type"];
-
-                if(employeeType)
-                {
-            		employeeType.employeeTypeId = employeeType.id;
-            		delete employeeType.id;
-                	payLoadContent.attributes["employeeType"] = employeeType;
-                }
-                delete payLoadContent.relationships;
-            }
-        }
-        console.log({ data: payLoadContent });
-        return { data: payLoadContent } ;
+    /*
+	For some reason, currently the default serializer will treat relationship employee type as 'employee-type'
+	It should be 'employeeType'.
+	Need to override the keyForRelationship, and simply return the passed in key.
+    */
+    keyForRelationship(key) {
+        return key; 
     }
 });
